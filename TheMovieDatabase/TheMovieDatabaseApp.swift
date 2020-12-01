af2c6605f11d6f4e05782548f8d3c136bcc19db5
Victor Mihaita <victormihaita@artlandapp.com>
2020-12-01 14:41:09 +0200

    Initial Commit

---
+++ TheMovieDatabase/TheMovieDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDatabaseApp.swift
+//  TheMovieDatabase
+//
+//  Created by Victor Mihaita on 01/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
